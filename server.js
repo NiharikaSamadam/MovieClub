0fe6fced2fea7182a63c4a8bebe86c70a105a839
niharika <samadamniharika@gmail.com>
2021-04-12 12:52:57 +0530

    server port changed

--- server.js
+++ server.js
@@ -48,5 +48,5 @@ app.use((err,req,res,next)=>{
 
 
 //assign port
-const port = process.env.PORT||4000
+const port = process.env.PORT||8080
 app.listen(port,() => console.log(`server started at port on ${port}`))
